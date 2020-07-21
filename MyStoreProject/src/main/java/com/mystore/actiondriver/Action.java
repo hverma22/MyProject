f572f7c2f4220c3ed77c89c5b1c3501d94e6774e
Hitendra <Hitendra@Hitendra-PC>
2020-07-21 22:55:43 +0530

    updated extent-config.xml and Action class

--- MyStoreProject/src/main/java/com/mystore/actiondriver/Action.java
+++ MyStoreProject/src/main/java/com/mystore/actiondriver/Action.java
@@ -762,7 +762,7 @@ public class Action extends BaseClass {
 			e.getMessage();
 		}
 		// This new path for jenkins
-		String newImageString = "http://localhost:8082/job/Demo4/ws/ExtentDemo/ScreenShot/" + filename + "_"
+		String newImageString = "http://localhost:8082/job/MyStoreProject/ws/MyStoreProject/ScreenShots/" + filename + "_"
 				+ dateName + ".png";
 		return destination;
 	}
