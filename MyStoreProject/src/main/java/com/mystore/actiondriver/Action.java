79ca742ffc69d038cc5b585fc772a76a806f3ad5
Hitendra <Hitendra@Hitendra-PC>
2020-07-22 11:46:03 +0530

    updated

--- MyStoreProject/src/main/java/com/mystore/actiondriver/Action.java
+++ MyStoreProject/src/main/java/com/mystore/actiondriver/Action.java
@@ -764,7 +764,7 @@ public class Action extends BaseClass {
 		// This new path for jenkins
 		String newImageString = "http://localhost:8082/job/MyStoreProject/ws/MyStoreProject/ScreenShots/" + filename + "_"
 				+ dateName + ".png";
-		return destination;
+		return newImageString;
 	}
 
 	public static String getCurrentTime() {
