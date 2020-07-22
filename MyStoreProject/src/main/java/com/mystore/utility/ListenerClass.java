ec0a1ed01fd9f6c52b9dc4370ab3140655bcdf8f
Hitendra <Hitendra@Hitendra-PC>
2020-07-22 12:08:56 +0530

    updated listener

--- MyStoreProject/src/main/java/com/mystore/utility/ListenerClass.java
+++ MyStoreProject/src/main/java/com/mystore/utility/ListenerClass.java
@@ -18,7 +18,6 @@ public class ListenerClass extends ExtentManager implements ITestListener {
 
 	public void onTestStart(ITestResult result) {
 		test = extent.createTest(result.getName());
-		test.createNode(result.getName());
 	}
 
 	public void onTestSuccess(ITestResult result) {
